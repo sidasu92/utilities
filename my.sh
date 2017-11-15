e340e732dd11d5f7c221ed8fd6c030752b488341
Sidhant <sidhant.sa@gmail.com>
2017-11-14 20:19:41 -0700

    removed pwd

--- my.sh
+++ my.sh
@@ -2,7 +2,7 @@
 /usr/bin/open -a "/Applications/Google Chrome.app" 'https://github.com/'
 /usr/bin/open -a "/Applications/Google Chrome.app" 'https://c9.io/'
 /usr/bin/open -a "/Applications/Google Chrome.app" 'https://ide.c9.io/sidasu92/my-uto-sfdc'
-/usr/bin/open -a "/Applications/Google Chrome.app" 'https://test.salesforce.com?un=sagarw45@asu.edu.sidhant&pw=caution16'
+/usr/bin/open -a "/Applications/Google Chrome.app" 'https://test.salesforce.com?un=sagarw45@asu.edu.sidhant&pw='
 /usr/bin/open -a "/Applications/Safari.app" 'https://webapp4.asu.edu/myasu/'
 /usr/bin/open -a "/Applications/Safari.app" 'https://asusf.slack.com/messages/'
 /usr/bin/open -a "/Applications/Safari.app" 'https://asudev.jira.com/secure/Dashboard.jspa'
